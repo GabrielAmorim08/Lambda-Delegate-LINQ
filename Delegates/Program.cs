114949737eec0d023adc6c5ee79bd4dd57f7b81e
Gabriel Amorim <gabriel.amorim.08@hotmail.com>
2024-02-20 16:15:44 -0300

    Introdução a Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿using Services;
+namespace Delegates;
+delegate double BinaryNumericOperation(double n1, double n2);
+class Program
+{
+    public void  Main(string[] args)
+    {
+        double a = 10;
+        double b =12;
+        BinaryNumericOperation op = CalculationService.Sum;
+        BinaryNumericOperation opOtherSintax= new BinaryNumericOperation(CalculationService.Max);
+         
+        double result = op.Invoke(a,b);
+        System.Console.WriteLine(result);
+    }
+}

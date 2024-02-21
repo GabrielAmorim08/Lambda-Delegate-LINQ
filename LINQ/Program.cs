cc57d6bfe2ac731db8bb6a8e6815eb6c9dbf763f
Gabriel Amorim <gabriel.amorim.08@hotmail.com>
2024-02-21 11:35:30 -0300

    Introdução a LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Linq;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Specify the data source
+        int[] numbers = new int[] {2,3,4,5};
+
+        //define the query expression
+        var result = numbers.Where(x => x%2 == 0).Select(x => x*10);
+
+        //Execute the query
+        foreach (int x in result)
+        {
+            System.Console.WriteLine(x);
+        }
+    }
+}

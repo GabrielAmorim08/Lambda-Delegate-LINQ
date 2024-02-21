af431c9e5ada31c76fead9bd8b9c75697ef005cd
Gabriel Amorim <gabriel.amorim.08@hotmail.com>
2024-02-21 10:19:52 -0300

    Estudando sobre a função Action

---
+++ Action/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace Entities;
+
+class Product
+{
+    //prop da classe
+    public string Name { get; set; }
+    public double Price { get; set; }
+    //construtor
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+    //metodo ToString
+    public override string ToString()
+    {
+        return Name +", " + Price.ToString("F2");
+    }
+}
---
+++ Action/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Entities;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //instanciando uma list Product
+        List<Product> list = new List<Product>();
+        //Adicionando itens a lista
+        list.Add(new Product("Tv", 900.00));
+        list.Add(new Product("Mouse", 50.00));
+        list.Add(new Product("Tablet", 350.50));
+        list.Add(new Product("Hd Case", 80.00));
+
+
+        //Versões para aumentar 10% o valor dos produtos
+        //utilizando o ForEach dentro da list, passando minha função void como parametro
+        list.ForEach(UpdatePrice);
+        //Criando um Action passando 1 parametro somente
+        Action<Product> act = UpdatePrice;
+        list.ForEach(act);
+        //Função lambda dentro do act
+        Action<Product> actL = p => {p.Price =+ p.Price *0.1;};
+        //Ou podemos utilizar a função lambda dentro do ForEach, nesse caso não precisamos definir uma outra função de fora
+        list.ForEach( p => {p.Price += p.Price *0.1;});
+
+        
+        foreach (Product p in list)
+        {
+            System.Console.WriteLine(p);
+        }
+    }
+    static void UpdatePrice(Product p)
+    {
+        p.Price += p.Price*0.1;
+    }
+}
+
